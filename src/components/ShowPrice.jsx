4ea8c02c141f7f60b20ee1a299660e234a5e2289
mcekov <mtsekov@zohomail.eu>
2023-10-25 18:33:15 +0300

    Add fuel price styles and data from API

--- src/components/ShowPrice.jsx
+++ src/components/ShowPrice.jsx
@@ -7,19 +7,16 @@ const Price = () => {
   const { fuelData } = useContext(FuelExpenseContext);
 
   return (
-    <>
-      {/*  <h4>Current {fuelData.fuel} Price</h4>
-      <h1 id="balance">{`${fuelData ? fuelData.price : "N/A"}${
-        fuelData ? fuelData.dimension : ""
-      }`}</h1> */}
-
+    <div>
       <div className="relative h-[90px] flex rounded-md border-gray-200 bg-white bg-clip-border shadow-md shadow-[#e9e9e9] dark:border-[#ffffff33] dark:!bg-navy-800 dark:text-white dark:shadow-none">
         <div className="h-50 ml-4 flex w-auto flex-col mt-2">
           <p className="font-dm text-sm font-medium text-gray-600">
-            Fuel price
+            {fuelData.fuel.toUpperCase()} FUEL PRICE
           </p>
           <p className="text-3xl font-bold text-navy-700 dark:text-white">
-            340.5 litter
+            {`${fuelData ? fuelData.price : "N/A"} ${
+              fuelData ? fuelData.dimension : ""
+            }`}
           </p>
         </div>
         <div className="ml-auto flex w-auto items-top pt-2 pr-2">
@@ -28,7 +25,7 @@ const Price = () => {
           </span>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
