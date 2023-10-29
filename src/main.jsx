ece61b1170beab2aa0c2b9535e3da99eee37afeb
mcekov <mtsekov@zohomail.eu>
2023-10-29 22:34:15 +0200

    FuelExpenseContext fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import { FuelExpenseProvider } from "./context/FuelExpenseContext.jsx";
+import { FuelExpenseProvider } from "./context/FuelExpenseContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <FuelExpenseProvider>
